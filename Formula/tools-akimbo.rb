68d2dece8122da1fa38e01587ba7f7e7f30d479c
Jeffrey Sambells <jeffrey.sambells@quarter4.io>
2024-05-02 00:39:41 -0400

    Updated tools-akimbo.rb sha

--- Formula/tools-akimbo.rb
+++ Formula/tools-akimbo.rb
@@ -3,10 +3,10 @@
 class ToolsAkimbo < Formula
     desc "CLI package of useful developer tools"
     homepage "https://jeffreysambells.com"
-    url "https://github.com/iamamused/homebrew-tools-akimbo/raw/1.0.1/phar/tools-akimbo.phar"
+    url "https://github.com/iamamused/homebrew-tools-akimbo/raw/1.0.2/phar/tools-akimbo.phar"
 
     # get sha: shasum -a 256 tools-akimbo.phar
-    sha256 "${SHA}"
+    sha256 "6e3efec1bf2164648eb826750060914771b45d67cf963e5e678e16e193edc4a0"
 
     depends_on "php"
 
