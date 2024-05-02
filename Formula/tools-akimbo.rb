09961d8260f46be64a75ed758d584aa51c102a95
Jeffrey Sambells <jeffrey.sambells@quarter4.io>
2024-05-01 23:50:19 -0400

    Update formula

--- Formula/tools-akimbo.rb
+++ Formula/tools-akimbo.rb
@@ -1,15 +1,17 @@
+# https://github.com/syhw/homebrew/blob/master/Library/Contributions/example-formula.rb
+
 class ToolsAkimbo < Formula
     desc "CLI package of useful developer tools"
-    homepage "https://github.com/iamamuse/homebrew-tools-skimbo"
+    homepage "https://jeffreysambells.com"
     url "https://github.com/iamamused/homebrew-tools-akimbo/raw/1.0.0/phar/tools-akimbo.phar"
 
     # get sha: shasum -a 256 tools-akimbo.phar
     sha256 "504a803617c935d056829753bcbb719d29c85ba6a3d1a347c9d37a5db0eb936e"
 
-    depends_on "php@8.2"
+    depends_on :php
 
     def install
-        bin.install "tools-akimbo.phar"
+        bin.install "tools-akimbo.phar" => "tools-akimbo"
     end
 
     def caveats; <<-EOS
